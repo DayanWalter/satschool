cd74b97bd6170c9615ea3c228573d4c81739e150
Dayan Walter <dayanwalter@gmail.com>
2024-03-14 06:38:06 +0100

    Added CartIcon to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Icon from "@mdi/react";
+import { mdiCartOutline } from "@mdi/js";
 
 export default function Header() {
   return (
-    <div className='h-24 bg-gray-400 flex'>
-      <div className='hidden sm:block'>Logo</div>
-      <div>
+    <div className='h-24  flex justify-between items-end pb-5 px-5 border-b'>
+      <div className='hidden sm:block text-4xl font-bold'>SatSch</div>
+      <div className='flex sm:gap-10 xl:gap-20'>
         <Button asChild variant='link'>
           <Link href='/'>Home</Link>
         </Button>
@@ -18,7 +20,14 @@ export default function Header() {
         </Button>
       </div>
 
-      <div>ShoppingCart</div>
+      <div>
+        {" "}
+        <Button asChild variant='link'>
+          <Link href='/cart'>
+            <Icon path={mdiCartOutline} size={1} />
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 }
