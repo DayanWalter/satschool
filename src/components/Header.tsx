7364e9f8463ab6916d2fa50e3549bc002c42c43d
Dayan Walter <dayanwalter@gmail.com>
2024-03-14 06:57:35 +0100

    Added Cartcount

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+
 import Link from "next/link";
 import Icon from "@mdi/react";
 import { mdiCartOutline } from "@mdi/js";
 
 export default function Header() {
+  const [cartcount, setCartcount] = useState(2);
+
   return (
-    <div className='h-24  flex justify-between items-end pb-5 px-5 border-b'>
+    <div className='h-24  flex justify-between items-end pb-5 px-5 xl:px-20 border-b'>
       <div className='hidden sm:block text-4xl font-bold'>SatSch</div>
       <div className='flex sm:gap-10 xl:gap-20'>
         <Button asChild variant='link'>
@@ -24,7 +29,12 @@ export default function Header() {
         {" "}
         <Button asChild variant='link'>
           <Link href='/cart'>
-            <Icon path={mdiCartOutline} size={1} />
+            <div className='relative'>
+              <Icon path={mdiCartOutline} size={1} />
+              <div className=' -top-2 -right-2 flex items-center justify-center rounded-full h-5 w-5 absolute bg-destructive text-destructive-foreground'>
+                {cartcount}
+              </div>
+            </div>
           </Link>
         </Button>
       </div>
