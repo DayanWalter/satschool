897e2a3e2d9a6335e644f666180f35757404ff8f
Dayan Walter <dayanwalter@gmail.com>
2024-03-14 06:22:43 +0100

    Header Buttons are now links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className='h-24 bg-gray-400 flex'>
       <div className='hidden sm:block'>Logo</div>
       <div>
-        <Button variant='link'>Home</Button>
-        <Button variant='link'>Contact</Button>
-        <Button variant='link'>About</Button>
+        <Button asChild variant='link'>
+          <Link href='/'>Home</Link>
+        </Button>
+        <Button asChild variant='link'>
+          <Link href='/contact'>Contact</Link>
+        </Button>
+        <Button asChild variant='link'>
+          <Link href='/about'>About</Link>
+        </Button>
       </div>
-      <div>Searchbar</div>
+
       <div>ShoppingCart</div>
     </div>
   );
