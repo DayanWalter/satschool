da5f2f7efe42c7d87083a0fdf69ff04933143cb2
Dayan Walter <dayanwalter@gmail.com>
2024-03-14 09:03:21 +0100

    Styled Subscribe section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { Input } from "@/components/ui/input";
+
 import Link from "next/link";
 import Icon from "@mdi/react";
 import { mdiHeartOutline } from "@mdi/js";
@@ -11,20 +13,20 @@ import { mdiLinkedin } from "@mdi/js";
 export default function Footer() {
   return (
     <>
-      <div className='h-96 text-white bg-black flex'>
-        <div>
-          <h1>SatSch</h1>
-          <h2>Subscribe</h2>
-          <p>Get 10% off your first order</p>
-          <input type='text' placeholder='Enter your email' />
+      <div className='min-h-96 dark:text-white dark:bg-black flex flex-col'>
+        <div className='border p-5'>
+          <h1 className='text-xl font-semibold mb-2'>SatSch</h1>
+          <h2 className='text-lg mb-2'>Subscribe</h2>
+          <p className='text-xs mb-2'>Get 10% off your first order</p>
+          <Input type='text' placeholder='Enter your email' />
         </div>
-        <div>
+        <div className='border p-5'>
           <h1>Support</h1>
           <p>Adress</p>
           <p>Emaildress</p>
           <p>Phone Number</p>
         </div>
-        <div>
+        <div className='border p-5'>
           <h1>Account</h1>
           <Button asChild variant='link'>
             <Link href='/cart'>Cart</Link>
@@ -33,7 +35,7 @@ export default function Footer() {
             <Link href='/'>Shop</Link>
           </Button>{" "}
         </div>
-        <div>
+        <div className='border p-5'>
           <h1>Quick Link</h1>
           <Button asChild variant='link'>
             <Link href='/privacy'>Privacy Policy</Link>
@@ -48,7 +50,7 @@ export default function Footer() {
             <Link href='/contact'>Contact</Link>
           </Button>{" "}
         </div>
-        <div>
+        <div className='border p-5'>
           <div>
             <h1>Donate Developer</h1>
             <Icon path={mdiHeartOutline} size={1} />
@@ -71,7 +73,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className='h-16 bg-black text-white border-t border-primary flex items-center justify-center'>
+      <div className='h-16 dark:bg-black dark:text-white border-t border-primary flex items-center justify-center'>
         <p>COPY</p>
       </div>
     </>
