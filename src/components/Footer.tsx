18726f962abae5ffcdb1d981adc21c9fbd08a290
Dayan Walter <dayanwalter@gmail.com>
2024-03-15 05:42:19 +0100

    Added Image comp to Donate section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,75 +9,78 @@ import { mdiFacebook } from "@mdi/js";
 import { mdiTwitter } from "@mdi/js";
 import { mdiInstagram } from "@mdi/js";
 import { mdiLinkedin } from "@mdi/js";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <>
-      <div className='flex border-t min-h-96 flex-col dark:bg-black dark:text-white md:flex-row md:justify-between md:p-8'>
-        <div className=' p-5'>
+      <div className='flex border-t min-h-96 flex-col dark:bg-black dark:text-white lg:flex-row lg:justify-between lg:px-16 lg:py-12'>
+        {/* Subscribe */}
+        <div className=' max-w-56 p-5'>
           <h1 className='mb-2 text-xl font-semibold'>SatSch</h1>
           <h2 className='mb-2 text-lg'>Subscribe</h2>
           <p className='mb-2 text-xs'>Get 10% off your first order</p>
           <Input type='text' placeholder='Enter your email' />
         </div>
+        {/* Support */}
         <div className=' p-5'>
           <h1 className='mb-2 text-xl font-semibold'>Support</h1>
-          <p className='mb-2 text-xs'>
-            25 Avenida Norte, San Salvador, El Salvador
-          </p>
-          <p className='mb-2 text-xs'>juan.perez@example.com</p>
+          <p className=' text-xs'>25 Avenida Norte,</p>
+          <p className=' text-xs'>San Salvador,</p>
+          <p className='mb-2 text-xs'>El Salvador</p>
+          <p className='mb-2 text-xs'>sat.nak@satsch.com</p>
           <p className='mb-2 text-xs'>(+503) 2222-3333</p>
         </div>
-        <div className=' p-5'>
+        {/* Account */}
+        <div className=' max-w-sm p-5'>
           <h1 className='mb-2 text-xl font-semibold'>Account</h1>
-          <div className='flex flex-col'>
+          <div className='flex flex-col '>
             <Link
-              className='mb-2 text-primary underline-offset-4 hover:underline'
+              className='mb-2  underline-offset-4 hover:underline'
               href='/cart'
             >
               Cart
             </Link>
-            <Link
-              className='text-primary underline-offset-4 hover:underline'
-              href='/'
-            >
+            <Link className=' underline-offset-4 hover:underline' href='/'>
               Shop
             </Link>
           </div>
         </div>
-        <div className=' p-5'>
+        {/* Quick Link */}
+        <div className=' max-w-sm p-5'>
           <h1 className='mb-2 text-xl font-semibold'>Quick Link</h1>
           <div className='flex flex-col'>
             <Link
-              className='mb-2 text-primary underline-offset-4 hover:underline'
+              className='mb-2  underline-offset-4 hover:underline'
               href='/privacy'
             >
               Privacy Policy
             </Link>
 
             <Link
-              className='mb-2 text-primary underline-offset-4 hover:underline'
+              className='mb-2  underline-offset-4 hover:underline'
               href='/terms'
             >
               Terms Of Use
             </Link>
 
             <Link
-              className='mb-2 text-primary underline-offset-4 hover:underline'
+              className='mb-2  underline-offset-4 hover:underline'
               href='/faq'
             >
               FAQ
             </Link>
 
             <Link
-              className='mb-2 text-primary underline-offset-4 hover:underline'
+              className='mb-2  underline-offset-4 hover:underline'
               href='/contact'
             >
               Contact
             </Link>
           </div>
         </div>
-        <div className=' p-5'>
+        {/* Donate Developer */}
+        <div className=' w-72 p-5'>
           <div className='mb-2 flex gap-3'>
             <h1 className='text-xl font-semibold'>Donate Developer</h1>
             <Icon path={mdiHeartOutline} size={1} />
@@ -85,18 +88,32 @@ export default function Footer() {
           <p className='mb-2 text-xs'>
             Send the developer some sats if you want
           </p>
-          <div className='mb-2 flex'>
-            <div className='w-1/2'>
-              <img className='max-w-24' src='./lnadress.jpg' alt='' />
+          <div className='mb-6 flex'>
+            <div className='w-1/2 flex justify-center'>
+              <div className='w-24 h-24 relative '>
+                <Image fill sizes='100px' priority src='/lnadress.jpg' alt='' />
+              </div>
             </div>
-            <div className='flex w-1/2 flex-col items-center justify-center gap-3'>
-              <Link href='https://google.com/' target='_blank'>
-                <img className='max-h-10 ' src='./google.svg' alt='' />
-              </Link>
 
-              <Link href='https://apple.com/' target='_blank'>
-                <img className='max-h-10' src='./apple.svg' alt='' />
-              </Link>
+            <div className='flex w-1/2 flex-col items-center justify-center'>
+              <div className=' relative w-24 h-12'>
+                <Link
+                  className=' absolute w-full h-full'
+                  href='https://google.com/'
+                  target='_blank'
+                >
+                  <Image fill src='/google.svg' alt='' />
+                </Link>
+              </div>
+              <div className='relative w-24 h-12'>
+                <Link
+                  className=' absolute w-full h-full'
+                  href='https://apple.com/'
+                  target='_blank'
+                >
+                  <Image fill src='/apple.svg' alt='' />
+                </Link>
+              </div>
             </div>
           </div>
           <div className='flex justify-between'>
@@ -115,7 +132,8 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className=' text-gray-500 flex h-16 items-center justify-center border-t border-primary dark:bg-black dark:text-white'>
+      {/* Licence */}
+      <div className=' text-gray-500  text-sm  flex h-16 items-center justify-center border-t border-primary dark:bg-black dark:text-white'>
         <p className='mr-1 hidden sm:block'>2024</p>
         <Link href='https://dayanwalter.com/' target='_blank'>
           <p className='mr-1 hidden sm:block'>Dayan Walter.</p>
