6f28e519f6c9b98bab7d183ecc6d4cf5ae58d909
Dayan Walter <dayanwalter@gmail.com>
2024-03-17 09:12:43 +0100

    Added cart.length to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,41 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 
 import Link from "next/link";
 import Icon from "@mdi/react";
 import { mdiCartOutline } from "@mdi/js";
 import { ModeToggle } from "./ModeToggle";
+import { useCart } from "./CartContext";
 
 export default function Header() {
-  const [cartcount, setCartcount] = useState(2);
+  const { cart } = useCart();
 
   return (
-    <div className='flex  h-24 items-end justify-between border-b px-5 pb-5 xl:px-20'>
-      <div className='hidden text-4xl font-bold sm:block'>
-        <Link href='/'>SatSch</Link>
+    <div className="flex  h-24 items-end justify-between border-b px-5 pb-5 xl:px-20">
+      <div className="hidden text-4xl font-bold sm:block">
+        <Link href="/">SatSch</Link>
       </div>
-      <div className='flex sm:gap-10 xl:gap-20'>
-        <Button asChild variant='link'>
-          <Link href='/'>Home</Link>
+      <div className="flex sm:gap-10 xl:gap-20">
+        <Button asChild variant="link">
+          <Link href="/">Home</Link>
         </Button>
-        <Button asChild variant='link'>
-          <Link href='/contact'>Contact</Link>
+        <Button asChild variant="link">
+          <Link href="/contact">Contact</Link>
         </Button>
-        <Button asChild variant='link'>
-          <Link href='/about'>About</Link>
+        <Button asChild variant="link">
+          <Link href="/about">About</Link>
         </Button>
       </div>
 
-      <div className='flex sm:gap-5'>
+      <div className="flex sm:gap-5">
         {" "}
-        <Button asChild variant='link'>
-          <Link href='/cart'>
-            <div className='relative'>
+        <Button asChild variant="link">
+          <Link href="/cart">
+            <div className="relative">
               <Icon path={mdiCartOutline} size={1} />
-              <div className=' absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-600 text-white'>
-                {cartcount}
+              <div className=" absolute -right-2 -top-2 flex h-5 w-5 items-center justify-center rounded-full bg-red-600 text-white">
+                {cart.length}
               </div>
             </div>
           </Link>
