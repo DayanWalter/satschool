d15987f3b5a1c6905aaf678d9bcec9fb44b12010
Dayan Walter <dayanwalter@gmail.com>
2024-03-19 03:56:09 +0100

    Changed Cart section to shadcn table

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -10,6 +10,17 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableFooter,
+} from "@/components/ui/table";
+
 import { useCart } from "@/components/CartContext";
 import Image from "next/image";
 
@@ -33,10 +44,18 @@ export default function Cart() {
     const subTotal = product.quantityInCart * product.price;
     return subTotal.toFixed(2);
   };
+  const calculateTotalAmount = () => {
+    const totalAmount = cart.reduce(
+      (total, product) => total + product.price * product.quantityInCart,
+      0,
+    );
+    return totalAmount.toFixed(2);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex w-full flex-col">
+        {/* Navigation Breadcrumb */}
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
@@ -51,49 +70,62 @@ export default function Cart() {
           </BreadcrumbList>
         </Breadcrumb>
         {/* Cart Content */}
-        <div>
-          {/* Cart Header */}
-          <div className="mb-5 grid grid-cols-4 rounded border px-5 py-2">
-            <p>Product</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Subtotal</p>
-          </div>
-          {/* Cart Item */}
-
-          {cart.map((product) => (
-            <div
-              key={product.id}
-              className="mb-2 grid grid-cols-4 rounded border p-5"
-            >
-              {/* Product */}
-              <div className="flex items-center gap-3">
-                <div className="relative h-12 w-12 ">
-                  <Image
-                    src={product.image}
-                    alt="product image"
-                    sizes="100%"
-                    fill
-                    className="object-contain"
-                  />
-                </div>
-                <p className="truncate">{product.title}</p>
-              </div>
-              {/* Price */}
-              <p>${product.price.toFixed(2)}</p>
-              {/* Quantity */}
-              <div className="flex w-16 items-center justify-between rounded border px-3 ">
-                <p>{product.quantityInCart}</p>
-                <div className="flex flex-col">
-                  <button onClick={() => handleIncrement(product.id)}>+</button>
-                  <button onClick={() => handleDecrement(product.id)}>-</button>
-                </div>
-              </div>
-              {/* Subtotal */}
-              <p>${calculateSubTotal(product)}</p>
-            </div>
-          ))}
-        </div>
+        <Table>
+          <TableCaption>A list of your items in your cart</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Product</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {cart.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell className="font-medium">
+                  <div className="flex items-center gap-3">
+                    <div className="relative h-12 w-12 ">
+                      <Image
+                        src={product.image}
+                        alt="product image"
+                        sizes="100%"
+                        fill
+                        className="object-contain"
+                      />
+                    </div>
+                    <p className="truncate">{product.title}</p>
+                  </div>
+                </TableCell>
+                <TableCell>${product.price.toFixed(2)}</TableCell>
+                <TableCell>
+                  <div className="flex w-16 items-center justify-between rounded border px-3 ">
+                    <p>{product.quantityInCart}</p>
+                    <div className="flex flex-col">
+                      <button onClick={() => handleIncrement(product.id)}>
+                        +
+                      </button>
+                      <button onClick={() => handleDecrement(product.id)}>
+                        -
+                      </button>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell className="text-right">
+                  ${calculateSubTotal(product)}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total</TableCell>
+              <TableCell className="text-right">
+                ${calculateTotalAmount()}
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
       </div>
       Cart Page
     </main>
