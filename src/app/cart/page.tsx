e0fcb3f09610c1234231ccf07c15b5a7dfe95d22
Dayan Walter <dayanwalter@gmail.com>
2024-03-19 04:24:10 +0100

    Added checkout button and function

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -51,6 +51,23 @@ export default function Cart() {
     );
     return totalAmount.toFixed(2);
   };
+  const checkout = async () => {
+    await fetch("http://localhost:3000/api/checkout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ products: cart }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.url) {
+          window.location.href = response.url;
+          // console.log(response.url)
+        }
+      });
+  };
 
   return (
     <>
@@ -132,6 +149,12 @@ export default function Cart() {
               </TableRow>
             </TableFooter>
           </Table>
+          <button
+            onClick={checkout}
+            className="mt-2 rounded border border-black px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:ring-offset-2"
+          >
+            Buy now
+          </button>
         </div>
       </main>
     </>
