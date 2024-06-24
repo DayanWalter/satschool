71b7e09918562bca5dd830e23ba822cac35594d6
Dayan Walter <dayanwalter@gmail.com>
2024-06-24 04:13:28 +0200

    Changed background color from StatisticsCard

--- src/app/about/StatisticsCard.tsx
+++ src/app/about/StatisticsCard.tsx
@@ -17,7 +17,8 @@ export default function StatisticsCard({
       <Card className="size-48 hover:bg-secondary">
         <CardContent>
           <div className="flex flex-col items-center p-5">
-            <div className="relative mb-7 size-12 rounded-full border-4 bg-primary">
+            {/* TODO: Fine tune background for different themes */}
+            <div className="relative mb-7 size-12 rounded-full border-4 bg-white">
               <Image
                 src={statistic.icon}
                 alt="statistic image"
